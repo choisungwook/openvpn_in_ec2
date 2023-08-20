c78ffb429fc94c51e9a3123183621b96f2647ddc
Choisungwook <kgg1959@naver.com>
2023-08-20 17:34:38 +0900

    openvpn ec2 instance securty group에 nginx port추가

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -39,6 +39,14 @@ resource "aws_security_group" "openvpn" {
   }
 
   ingress {
+    description = "http"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     description = "openvpn server"
     from_port   = 1194
     to_port     = 1194
